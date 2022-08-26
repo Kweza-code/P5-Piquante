3e422d5f2148b695c91ac8916faadf7feae8006a
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-08-26 14:20:12 +0200

    Mise à jour de controllers/Useer.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
-const Thing = require('./models/thing');
+const userRoutes = require('./routes/user');                                                          
 const app = express();
 
 mongoose.connect('mongodb+srv://france693:france693@cluster0.j8bhm1r.mongodb.net/?retryWrites=true&w=majority',
@@ -23,58 +22,6 @@ app.use((req, res, next) => {
     next();
   });
 
-//middleware POST
-  app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
-  });
-
-
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
-
-//save of things in base 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-// Getting special thing
-  app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-// Getting things
-  app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
-
+  //Racine de la routes d'authentification
+  app.use('/api/auth', userRoutes);
   module.exports = app;
