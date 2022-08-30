0d880738505eb5cbe1630c426d32cd6d40997b54
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-08-30 20:45:16 +0200

    maj

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb+srv://france693:france693@cluster0.j8bhm1r.mongodb.net
 app.use(express.json());
 
 
-//Cross Origin Ressource Sharing
+//header of the global acess of the api
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
