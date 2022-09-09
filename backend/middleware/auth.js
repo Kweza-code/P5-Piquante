5b39158fe459f72d1859f227a33c214cd0032487
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-09-09 11:33:52 +0200

    Changement de Middleware/auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,15 @@
 const jwt = require('jsonwebtoken');
-
-
-
+ 
 module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1]
-    const decodedToken = jwt.verify(token, 'FUHZEFBEZFHBEAFBE')
-    const userId = decodedToken.userId
-    console.log('id from Auth')
-    console.log(userId)
-    req.auth = { userId}
-    next()
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
-  }
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, 'FUHZEFBEZFHBEAFBE');
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
 };
