9a5a598d9205b96bbabe7b00078caef07fca70c2
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-09-13 20:03:58 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,31 @@
-const express = require('express');
+const express = require("express");
 const helmet = require("helmet");
-const  mongoSanitize  =  require ( 'express-mongo-sanitize' ) ;
-const mongoose = require('mongoose');
-const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/users');                                                          
+const mongoSanitize = require("express-mongo-sanitize");
+const mongoose = require("mongoose");
+const saucesRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/users");
 const app = express();
-const dotenv = require('dotenv').config();
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
-console.log(process.env.SECRET_KEY)
+const dotenv = require("dotenv").config();
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+console.log(process.env.SECRET_KEY);
 
-mongoose.connect('mongodb+srv://france693:france693@cluster0.j8bhm1r.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    "mongodb+srv://france693:france693@cluster0.j8bhm1r.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
- // espress mongo sanitize
-  app.use(
-    mongoSanitize({
-      onSanitize: ({ req, key }) => {
-        console.warn(`This request[${key}] is sanitized`, req);
-      },
-    }),
-  );  
+// espress mongo sanitize
+app.use(
+  mongoSanitize({
+    onSanitize: ({ req, key }) => {
+      console.warn(`This request[${key}] is sanitized`, req);
+    },
+  })
+);
 // helmet
 app.use(
   helmet({
@@ -34,16 +36,21 @@ app.use(
 //Getting all contentType
 app.use(express.json());
 
-
 //header of the global acess of the api
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
-  //Racine de la routes d'authentification
-  app.use('/api/sauces', saucesRoutes);
-  app.use('/api/auth', userRoutes);
-  module.exports = app;
+//Racine de la routes d'authentification
+app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", userRoutes);
+module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 //associer les fonctions au differentes routes
-const userCtrl = require('../controllers/users');
+const userCtrl = require("../controllers/users");
 
-//creating routes post 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+//creating routes post
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
 
 module.exports = router;
